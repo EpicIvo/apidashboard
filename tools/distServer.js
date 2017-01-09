a08fd36d2a6c9ac7cc97600ff9e5ec4c14a59882
RandomDesigner <matthijsperik@gmail.com>
2017-01-09 14:55:57 +0100

    Changed static from distServer for Heroku

--- tools/distServer.js
+++ tools/distServer.js
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use('/v1', router);
-app.use(express.static(`${__dirname}/../public`));
+app.use(express.static(`${__dirname}/public`));
 app.use((req, res) => {
     return res.sendFile(path.resolve('public/index.html'));
 });
