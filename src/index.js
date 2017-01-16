b657cb847ca318a20f402ca2c196c85aae79af02
RandomDesigner <matthijsperik@gmail.com>
2017-01-16 17:27:25 +0100

    Removed jquery/bootstrap js.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
-import 'jquery/dist/jquery.min';
-import 'bootstrap/dist/js/bootstrap.min';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/styles.scss';
 
