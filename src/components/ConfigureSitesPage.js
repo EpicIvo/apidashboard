367d1a32cc10087f64c9e75a9ae506fadbdb78d8
RandomDesigner <matthijsperik@gmail.com>
2017-01-20 17:14:20 +0100

    Added remove button to ConfigureSitesPage domain list.

--- src/components/ConfigureSitesPage.js
+++ src/components/ConfigureSitesPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import update from 'react-addons-update';
 import SiteService from '../services/SiteService';
 
@@ -21,7 +21,7 @@ class ConfigureSitesPage extends Component {
     const id = this.props.params.id;
 
     this.Site.getById(id).then((res) => {
-      this.setState({ site: res.site });
+      this.setState({site: res.site});
     });
   }
 
@@ -30,13 +30,22 @@ class ConfigureSitesPage extends Component {
     const property = event.target.getAttribute('data-type');
     const key = event.target.getAttribute('data-key');
 
-    const updated = update(this.state.site, {
-      [property]: {
-        [key]: { $set: event.target.value }
-      }
-    });
+    let updated;
 
-    this.setState({ site: updated });
+    if (key) {
+      updated = update(this.state.site, {
+        [property]: {
+          [key]: {$set: event.target.value}
+        }
+      });
+    }
+    else {
+      updated = update(this.state.site, {
+        [property]: {$set: event.target.value}
+      });
+    }
+
+    this.setState({site: updated});
   }
 
   addPath() {
@@ -44,14 +53,14 @@ class ConfigureSitesPage extends Component {
     let site = this.state.site;
 
     site.paths.push([]);
-    this.setState({ site: site });
+    this.setState({site: site});
   }
 
   addDomain() {
 
     let site = this.state.site;
     site.domains.push([]);
-    this.setState({ site: site });
+    this.setState({site: site});
   }
 
   saveSite() {
@@ -59,8 +68,13 @@ class ConfigureSitesPage extends Component {
     let site = this.state.site;
 
     this.Site.save(site).then((res) => {
-        console.log(res);
+      console.log(res);
     });
+  }
+
+  remove() {
+
+    //TODO : Remove either domain or path
   }
 
   render() {
@@ -69,13 +83,16 @@ class ConfigureSitesPage extends Component {
         { this.state.site && <div>
           <div>
             <h3>Site <span className="btn btn-success" onClick={() => this.saveSite()}>Save site</span></h3>
-            <input type="text" className="form-control" defaultValue={this.state.site.project} />
+            <input type="text" className="form-control" data-type="project" value={this.state.site.project}
+                   onChange={this.handleChange}/>
           </div>
           <div>
             <h3>Domains <span className="btn btn-primary" onClick={() => this.addDomain()}>Add domain</span>
             </h3>
-            { this.state.site.domains.map((domain, index) => <div key={index}>
-                <input type="text" className="form-control domain" data-type="domains" data-key={index} value={domain} onChange={this.handleChange} />
+            { this.state.site.domains.map((domain, index) => <div className="input-group" key={index}>
+                <input type="text" className="form-control domain" data-type="domains" data-key={index} value={domain}
+                       onChange={this.handleChange}/>
+                <span className="input-group-addon btn btn-danger" onClick={() => this.remove()}>Remove</span>
               </div>
             )}
           </div>
@@ -83,7 +100,8 @@ class ConfigureSitesPage extends Component {
             <h3>Paths <span className="btn btn-primary" onClick={() => this.addPath()}>Add domain</span>
             </h3>
             { this.state.site.paths.map((path, index) => <div key={index}>
-                <input type="text" className="form-control" data-type="paths" data-key={index} value={path} onChange={this.handleChange} />
+                <input type="text" className="form-control" data-type="paths" data-key={index} value={path}
+                       onChange={this.handleChange}/>
               </div>
             )}
           </div>
